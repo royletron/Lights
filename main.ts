f00c039be049dddced17633c4832f270d795117e
Darren <darren@ventureharbour.com>
2022-12-27 15:53:39 +0000

    Adding Test Lights

--- main.ts
+++ main.ts
@@ -1,11 +1,19 @@
-let X = 0
-scrollbit.setAll(0)
+input.onButtonPressed(Button.A, function () {
+    strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    strip.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
+    strip.show()
+})
+input.onButtonPressed(Button.B, function () {
+    strip.clear()
+    strip.show()
+})
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
 basic.forever(function () {
-    X += 1
-    scrollbit.setIcon(
-    IconNames.Heart,
-    X % 16,
-    0,
-    128
-    )
+    basic.pause(100)
+    strip.rotate(1)
+    strip.show()
 })
